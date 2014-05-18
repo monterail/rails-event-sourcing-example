068581c242dbc6687d885fa39086ffe57f2716c0
Tymon Tobolski <i@teamon.eu>
2014-05-18 02:13:37 +0200

    Recent posts limit fix

--- dashboard/app/services/recent_posts.rb
+++ dashboard/app/services/recent_posts.rb
@@ -3,13 +3,13 @@ class RecentPosts
   STORE_LIMIT = 5
 
   def self.list(limit = STORE_LIMIT)
-    $redis.lrange(KEY, 0, limit).map do |raw_post|
+    $redis.lrange(KEY, 0, limit-1).map do |raw_post|
       JSON.parse(raw_post).with_indifferent_access
     end
   end
 
   def self.push(raw_post)
     $redis.lpush(KEY, raw_post)
-    $redis.ltrim(KEY, 0, STORE_LIMIT)
+    $redis.ltrim(KEY, 0, STORE_LIMIT-1)
   end
 end
